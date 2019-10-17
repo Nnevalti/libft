8f100223612b0dc7f416a1a83aeb86fc4358be3d
Valentin DESCHAMPS <vdescham@e1r9p19.42.fr>
2019-10-17 19:59:15 +0200

    idk but it's a serious commit

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: vdescham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 16:35:28 by vdescham          #+#    #+#             */
-/*   Updated: 2019/10/16 17:56:03 by vdescham         ###   ########.fr       */
+/*   Updated: 2019/10/17 12:13:20 by vdescham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char			*mem;
 	unsigned int	i;
 
-	if (!(mem = malloc((len + 1) * sizeof(char))))
-		return ("NULL");
 	i = 0;
 	if (start > ft_strlen(s))
 		return ("\0");
+	if (!(mem = malloc((len + 1) * sizeof(char))))
+		return ("NULL");
 	while (len-- && s[start + i])
 	{
 		mem[i] = s[start + i];
