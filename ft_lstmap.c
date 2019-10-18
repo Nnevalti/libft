086327b923b883ad0776ad66a7a4e92e52a77601
Valentin DESCHAMPS <vdescham@e1r9p19.42.fr>
2019-10-18 13:17:12 +0200

    finish libft

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vdescham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/11 17:44:52 by vdescham          #+#    #+#             */
-/*   Updated: 2019/10/11 17:57:51 by vdescham         ###   ########.fr       */
+/*   Updated: 2019/10/18 12:11:03 by vdescham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *))
 {
 	t_list	*newlst;
 
+	newlst = NULL;
 	while (lst)
 	{
 		ft_lstadd_back(&newlst, ft_lstnew((*f)(lst->content)));
